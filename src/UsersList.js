47b2f6dcf9602a4638321affec09b601ce361058
Bruno Gurgel <bmg1612@gmail.com>
2021-02-07 12:08:24 -0300

    feat: Added UsersList component

---
+++ src/UsersList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GamesCount from "./GamesCount";
+
+function UsersList(props) {
+  return (
+    <div>
+      <ol></ol>
+      <GamesCount />
+    </div>
+  );
+}
+
+export default UsersList;
