1b4658f9d2f5739aa26688cd8f653ec61bf0df87
Bruno Gurgel <bmg1612@gmail.com>
2021-02-07 12:05:34 -0300

    feat: Added Button component

---
+++ src/Button.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Button(props) {
+  return (
+    <div>
+      <button onClick={props.deleteLastUser} disabled={props.disabled}>
+        {props.name}
+      </button>
+    </div>
+  );
+}
+
+Button.propTypes = {
+  disabled: PropTypes.bool.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+export default Button;
