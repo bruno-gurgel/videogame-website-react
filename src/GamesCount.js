8d0e1494d65d7d9abb5fcf720f0e3806446151ac
Bruno Gurgel <bmg1612@gmail.com>
2021-02-08 07:39:33 -0300

    feat: Added methods to GamesCount component

--- src/GamesCount.js
+++ src/GamesCount.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-function GamesCount() {
-  return <button>Hide/Show games played</button>;
+function GamesCount(props) {
+  return (
+    <button
+      className="smallButton"
+      onClick={props.toggleVisibility}
+      disabled={props.usersArray.length === 0}
+    >
+      {props.visibility ? "Hide games played" : "Show games played"}
+    </button>
+  );
 }
 
 export default GamesCount;
