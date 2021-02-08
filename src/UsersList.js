25f8222bab22a00c3763b408f8d58340ea0ef0f6
Bruno Gurgel <bmg1612@gmail.com>
2021-02-08 07:39:16 -0300

    feat: Added methods to UsersList component

--- src/UsersList.js
+++ src/UsersList.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import GamesCount from "./GamesCount";
 
-function UsersList(props) {
-  return (
-    <div>
-      <ol></ol>
-      <GamesCount />
-    </div>
-  );
+class UsersList extends Component {
+  state = {
+    visible: true,
+  };
+  toggleVisibility = () => {
+    this.state.visible
+      ? this.setState({
+          visible: false,
+        })
+      : this.setState({
+          visible: true,
+        });
+  };
+  render() {
+    return (
+      <div>
+        <ol>
+          {this.props.usersArray.map((user) => (
+            <li key={user.id}>
+              {user.userName}
+              {this.state.visible ? (
+                <span id="games-played">Games played: {user.gamesPlayed}</span>
+              ) : (
+                ""
+              )}
+            </li>
+          ))}
+        </ol>
+        <GamesCount
+          visibility={this.state.visible}
+          toggleVisibility={this.toggleVisibility}
+          usersArray={this.props.usersArray}
+        />
+      </div>
+    );
+  }
 }
 
 export default UsersList;
