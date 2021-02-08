b4ce3e91f30c1de5605ed68d169a5edcf7976050
Bruno Gurgel <bmg1612@gmail.com>
2021-02-08 07:38:10 -0300

    feat: Added methods to Form component

--- src/Form.js
+++ src/Form.js
@@ -4,13 +4,47 @@ import Button from "./Button";
 import PropTypes from "prop-types";
 
 class Form extends Component {
+  state = {
+    contactExists: false,
+  };
   static propTypes = {
     formSubmit: PropTypes.func.isRequired,
     handleChange: PropTypes.func.isRequired,
   };
+  inputsAreEmpty = () => {
+    return (
+      this.props.firstNameInput === "" ||
+      this.props.lasttNameInput === "" ||
+      this.props.userNameInput === ""
+    );
+  };
+  checkIfUserExists = (newUsername) => {
+    const users = this.props.users;
+    for (let user of users) {
+      if (user.userName === newUsername) {
+        return true;
+      }
+    }
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const userExists = this.checkIfUserExists(this.props.userNameInput);
+
+    if (!userExists) {
+      this.setState(() => ({
+        contactExists: false,
+      }));
+      this.props.formSubmit();
+    } else {
+      this.setState(() => ({
+        contactExists: true,
+      }));
+    }
+  };
   render() {
     return (
-      <form onSubmit={this.props.formSubmit}>
+      <form onSubmit={this.handleSubmit}>
         <Input
           name="firstName"
           placeholder="Your First Name"
@@ -26,6 +60,11 @@ class Form extends Component {
           placeholder="Choose an Username"
           handleChange={this.props.handleChange}
         />
+        {this.state.contactExists ? (
+          <p className="error">You cannot add a user that already exists.</p>
+        ) : (
+          ""
+        )}
         <Button name="Add User" disabled={this.inputsAreEmpty()} />
       </form>
     );
